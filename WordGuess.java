22b6a553a3f0e5ae55f8d5bddac9976e72d244a4
ayushiagrawal <Ayushi.Agrawal@cdk.com>
2016-07-27 15:09:26 +0530

    Add files via upload

--- WordGuess.java
+++ WordGuess.java
@@ -1,20 +1,23 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Random;
 
 /**
  * Created by agrawaay on 7/27/2016.
  */
 public class WordGuess {
- public static void main(String [] args) {
+ public static void main(String [] args) throws FileNotFoundException {
      String fileName = "C:\\Users\\agrawaay\\Documents\\sowpods.txt";
      FileReader fread = new FileReader(fileName);
      BufferedReader bufferedReader = new BufferedReader(fread);
      String line = "";
      ArrayList<String> lineArray = new ArrayList<>();
+     String myWord = "am";
 
      try {
          while((line = bufferedReader.readLine()) != null) {
@@ -27,6 +30,14 @@ public class WordGuess {
      }
      ArrayList<String > variable = new ArrayList<>();
      variable = wordsWithSizeTwo(lineArray);
+    /* for(String str : variable) {
+         System.out.println(str);
+     }*/
+     Random rand = new Random(System.currentTimeMillis());
+     String randomWord = variable.get(rand.nextInt(variable.size()));
+     char [] array = randomWord.toCharArray();
+     System.out.println(randomWord);
+
 
  }
     public static ArrayList<String> wordsWithSizeTwo(ArrayList<String> words) {
